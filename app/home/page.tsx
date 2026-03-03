e7b5b12ecb2a969a97f0c2e0ff020f8a7858c679
WilloS88 <ramon2003@email.cz>
2026-03-03 14:28:08 +0100

    Chakra UI implementation,Navbar component

---
+++ app/home/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/navbar";
+
+export default function Home() {
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
+
+            asdasdas
+        </div>
+    );
+}
